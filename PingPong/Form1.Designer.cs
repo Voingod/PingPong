0cb7d4b3067ad48b3d2ac470a47bf3c399ef700c
Voingod <43098874+Voingod@users.noreply.github.com>
2019-07-09 23:51:42 +0300

    Настройка логики игры

--- PingPong/Form1.Designer.cs
+++ PingPong/Form1.Designer.cs
@@ -28,53 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Field = new System.Windows.Forms.Panel();
-            this.pictureBox_StickToCatch = new System.Windows.Forms.PictureBox();
-            this.pictureBox_ElementForCatch = new System.Windows.Forms.PictureBox();
+            this.gamePanel = new System.Windows.Forms.PictureBox();
+            this.gameBall = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.loser = new System.Windows.Forms.Label();
             this.Field.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_StickToCatch)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ElementForCatch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBall)).BeginInit();
             this.SuspendLayout();
             // 
             // Field
             // 
-            this.Field.Controls.Add(this.pictureBox_ElementForCatch);
-            this.Field.Controls.Add(this.pictureBox_StickToCatch);
+            this.Field.Controls.Add(this.loser);
+            this.Field.Controls.Add(this.ScoreLabel);
+            this.Field.Controls.Add(this.gameBall);
+            this.Field.Controls.Add(this.gamePanel);
             this.Field.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Field.Location = new System.Drawing.Point(0, 0);
             this.Field.Name = "Field";
-            this.Field.Size = new System.Drawing.Size(800, 450);
+            this.Field.Size = new System.Drawing.Size(800, 374);
             this.Field.TabIndex = 0;
             // 
-            // pictureBox_StickToCatch
+            // gamePanel
             // 
-            this.pictureBox_StickToCatch.BackColor = System.Drawing.Color.Red;
-            this.pictureBox_StickToCatch.Location = new System.Drawing.Point(226, 402);
-            this.pictureBox_StickToCatch.Name = "pictureBox_StickToCatch";
-            this.pictureBox_StickToCatch.Size = new System.Drawing.Size(329, 21);
-            this.pictureBox_StickToCatch.TabIndex = 0;
-            this.pictureBox_StickToCatch.TabStop = false;
+            this.gamePanel.BackColor = System.Drawing.Color.Red;
+            this.gamePanel.Location = new System.Drawing.Point(339, 331);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(76, 21);
+            this.gamePanel.TabIndex = 0;
+            this.gamePanel.TabStop = false;
             // 
-            // pictureBox_ElementForCatch
+            // gameBall
             // 
-            this.pictureBox_ElementForCatch.BackColor = System.Drawing.Color.Blue;
-            this.pictureBox_ElementForCatch.Location = new System.Drawing.Point(121, 97);
-            this.pictureBox_ElementForCatch.Name = "pictureBox_ElementForCatch";
-            this.pictureBox_ElementForCatch.Size = new System.Drawing.Size(70, 63);
-            this.pictureBox_ElementForCatch.TabIndex = 1;
-            this.pictureBox_ElementForCatch.TabStop = false;
+            this.gameBall.BackColor = System.Drawing.Color.Blue;
+            this.gameBall.Location = new System.Drawing.Point(121, 137);
+            this.gameBall.Name = "gameBall";
+            this.gameBall.Size = new System.Drawing.Size(21, 23);
+            this.gameBall.TabIndex = 1;
+            this.gameBall.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(692, 9);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(96, 25);
+            this.ScoreLabel.TabIndex = 2;
+            this.ScoreLabel.Text = "Score: 0";
+            // 
+            // loser
+            // 
+            this.loser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loser.AutoSize = true;
+            this.loser.Font = new System.Drawing.Font("MV Boli", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loser.Location = new System.Drawing.Point(265, 137);
+            this.loser.Name = "loser";
+            this.loser.Size = new System.Drawing.Size(318, 63);
+            this.loser.TabIndex = 3;
+            this.loser.Text = "You are lose";
+            this.loser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 374);
             this.Controls.Add(this.Field);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.Field.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_StickToCatch)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ElementForCatch)).EndInit();
+            this.Field.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBall)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -82,8 +120,11 @@
         #endregion
 
         private System.Windows.Forms.Panel Field;
-        private System.Windows.Forms.PictureBox pictureBox_StickToCatch;
-        private System.Windows.Forms.PictureBox pictureBox_ElementForCatch;
+        private System.Windows.Forms.PictureBox gamePanel;
+        private System.Windows.Forms.PictureBox gameBall;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label loser;
     }
 }
 
--- PingPong/Form1.cs
+++ PingPong/Form1.cs
@@ -12,9 +12,77 @@ namespace PingPong
 {
     public partial class Form1 : Form
     {
+        private int speed_vertical = 5;
+        private int speed_horizontal = 5;
+        private int score = 0;
+
         public Form1()
         {
             InitializeComponent();
+            timer.Enabled = true;
+            Cursor.Hide();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            gamePanel.Top = Field.Bottom - Field.Bottom / 10;
+            loser.Top = (Field.Height / 2)-(loser.Height/2);
+            loser.Left = (Field.Width / 2)- (loser.Width/2);
+
+            loser.Visible = false;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+            if (e.KeyCode == Keys.R)
+            {
+                gameBall.Left = 70;
+                gameBall.Top = 60;
+                speed_horizontal = 5;
+                speed_vertical = 5;
+                score = 0;
+                loser.Visible = false;
+                ScoreLabel.Text = "Score: 0";
+                timer.Enabled = true;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            gamePanel.Left = Cursor.Position.X - (gamePanel.Width / 2);
+            gameBall.Left += speed_horizontal;
+            gameBall.Top += speed_vertical;
+
+            if (gameBall.Bottom >= Field.Bottom)
+            {
+                timer.Enabled = false;
+                loser.Visible = true;
+            }
+            if (gameBall.Top<=Field.Top)
+                speed_vertical *= -1;
+            if (gameBall.Left <= Field.Left)
+                speed_horizontal *= -1;
+            if (gameBall.Right >= Field.Right)
+                speed_horizontal *= -1;
+
+            if(gameBall.Left>=gamePanel.Left&&gameBall.Right<=gamePanel.Right
+                &&gameBall.Bottom >= gamePanel.Top&&gameBall.Bottom<=gamePanel.Bottom)
+            {
+                speed_vertical += 1;
+                speed_vertical *= -1;
+                score += 1;
+                ScoreLabel.Text = "Score: " + score.ToString();
+                Random colour_rnd = new Random();
+                int red = colour_rnd.Next(150, 255);
+                int green = colour_rnd.Next(150, 255);
+                int blue = colour_rnd.Next(150, 255);
+                Field.BackColor = Color.FromArgb(red,green,blue);
+            }
+
+
         }
     }
 }
